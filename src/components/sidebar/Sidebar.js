2063d7d6b4fcf68e356c7903403735327db6da87
Bhavesh Varu <bhavesh.varu97@gmail.com>
2024-07-28 22:23:55 +0530

    scafoldded app

---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './sidebar.css'
+
+const Sidebar = () => {
+  return <div className='side'>
+    <div className='side-section-1'>
+    
+    </div>
+
+  </div>
+}
+
+export default Sidebar
---
+++ src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>check the font</h1>
+    </div>
+  )
+}
+
+export default HomePage
