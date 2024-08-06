230ba875629b64191b135265bcbf4bb3f8619c61
Bhavesh Varu <bhavesh.varu97@gmail.com>
2024-08-06 22:26:34 +0530

    added footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,7 +7,34 @@ import { FaFacebook } from 'react-icons/fa'
 const Footer = () => {
   return (
     <div className='footer_container'>
-      <div className='links'></div>
+      <div className='links'>
+        <ul className='links_ui'>
+          <li>Company</li>
+          <li>About</li>
+          <li>Jobs</li>
+          <li>For the Record</li>
+        </ul>
+        <ul className='links_ui'>
+          <li>Communities</li>
+          <li>For Artists</li>
+          <li>Developers</li>
+          <li>Advertising</li>
+          <li>Investors</li>
+          <li>Vendors</li>
+        </ul>
+        <ul className='links_ui'>
+          <li>Useful Links</li>
+          <li>Support</li>
+          <li>Free Mobile App</li>
+        </ul>
+        <ul className='links_ui'>
+          <li>Spotify Plans</li>
+          <li>Premium Individual</li>
+          <li>Premium Duo</li>
+          <li>Premium Family</li>
+          <li>Premium student</li>
+        </ul>
+      </div>
       <div className='icons'>
         <div className='ico'>
           <FaInstagram size={25} color='white' />
